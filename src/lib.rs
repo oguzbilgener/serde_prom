214e06f07e8baa9497a8b6ebb6c4435ecd423b6c
Oguz Bilgener <oguz@bilgener.me>
2025-03-19 12:17:28 -0400

    chore: Suppress two clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![doc = include_str!("../README.md")]
-
+#![allow(clippy::doc_markdown)]
+#![allow(clippy::implicit_hasher)]
 pub use error::PrometheusError;
 pub use ser::{to_prometheus_text, write_prometheus_text};
 
